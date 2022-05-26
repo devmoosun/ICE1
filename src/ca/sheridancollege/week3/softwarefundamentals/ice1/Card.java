ec257769398e84e1ffbf71f3134fb9390dd6d8f0
devmoosun <moosundammy@gmail.com>
2022-05-26 16:25:32 -0400

    User Input

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -49,6 +49,7 @@ public class Card {
         this.value = value;
     }
    
+    
    
     
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -17,20 +19,41 @@ public class CardTrick {
     public static void main(String[] args)
            
     {
-         System.out.print("hello");
-         
+        
+
+
+
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
-        {
+        {   
+            Random rand = new Random();
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int randomNumber = rand.nextInt(13) + 1;
+            int randomNumberSuit = rand.nextInt(3) + 1;
+            c.setValue(randomNumber);
+            c.setSuit(Card.SUITS[randomNumberSuit]);
+            magicHand[i] = c;
+            
+            
+
         }
         
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter Card Value");
+        int cardValue = input.nextInt();
+        
+         System.out.println("Enter Card Suit \n 1: Hearts \n 2: Diamonds \n 3: Spades \n 4: Clubs"); 
+         int cardSuit = input.nextInt();
+         
+         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        
+        
     }
     
 }
