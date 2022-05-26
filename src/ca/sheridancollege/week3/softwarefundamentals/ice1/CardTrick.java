173dfbf0f7960a0dc52230bcc3f5956760897c80
devmoosun <moosundammy@gmail.com>
2022-05-26 15:18:56 -0400

    This is my first commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -14,7 +14,9 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 public class CardTrick {
     
     public static void main(String[] args)
+           
     {
+         
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
